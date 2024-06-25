6650a9e199d5d737507f6ecf8a9fc4b7fde64231
Automatisierung <auto.matisierung@swvk.de>
2024-06-25 08:44:05 +0200

    change csv url

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 # Step 1: Read the CSV file
-df = pd.read_csv('new_appointments.csv')  # Use relative path
+df = pd.read_csv(r'C:\Users\Vadim.Khablov\Downloads\ExportedFiles_8692edf1-0f2f-47de-8ed6-337ee1be6f4e\new_appointments.csv')
 
 # Convert the date columns to datetime
 df['new_starttime'] = pd.to_datetime(df['new_starttime'])
